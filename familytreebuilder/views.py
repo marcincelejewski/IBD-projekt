b2a65cf98a0f809424f5bb6099feb772d0db64d5
Marcin Celejewski <marcin.celejewski18@gmail.com>
2018-11-21 16:45:11 +0100

    zmodyfikowano logowanie

--- familytreebuilder/views.py
+++ familytreebuilder/views.py
@@ -1,13 +1,13 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
 
 
 class Signup(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'familytreebuilder/signup.html'
 
